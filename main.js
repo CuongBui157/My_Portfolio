4903ccec73f8bdf33f4451dae7096a024071c2ab
Cường Bùi <cuongbtph26367@fpt.edu.vn>
2023-02-19 21:26:33 +0700

    tao components cac thu

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-import "./src/css/index.css";
+import { router, render } from "./src/lib";
+import Header from "./src/components/home/Header";
+import Banner from "./src/components/home/Banner";
+import Skill from "./src/components/home/Skill";
+import Contact from "./src/components/home/Contact";
+
+const header = document.querySelector("#header");
+const banner = document.querySelector("#banner");
+const skill = document.querySelector("#skill");
+const contact = document.querySelector("#contact");
+
+router.on("", () => render(Header(), header));
+router.on("", () => render(Banner(), banner));
+router.on("", () => render(Skill(), skill));
+router.on("", () => render(Contact(), contact));
+
+router.resolve();
---
+++ src/components/home/Banner.js
@@ -0,0 +1,55 @@
+const Banner = () => {
+  return /*html*/ `
+        <div class="banner-hello">
+            <h1>Xin chào, tôi là Cường 🙋‍♂️</h1>
+        </div>
+        <div class="banner-img">
+            <div class="card">
+                <h1>ABOUT ME</h1>
+                <img
+                src="./src/img/z4117398338096_7ae3add938cd770784a70ed21f9c0bd7.jpg"
+                alt=""
+                />
+
+                <div class="content">
+                <h3>ABOUT ME</h3>
+
+                <p>
+                    Chào các bạn, tôi là Bùi Tiến Cường. Hiện tại tôi là sinh viên
+                    kỳ 5 tại FTP Polytechnic. <br />Tôi là một Front end Developer
+                    chịu trách nhiệm phát triển giao diện bên ngoài của một
+                    website dựa vào những bản thiết kế. <br />Email:
+                    cuongbui1572003@gmail.com <br />SĐT: +84 38 247 0415
+                </p>
+                <div class="content-item__link">
+                    <a href="">
+                        <img
+                            src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Facebook_Logo_%282019%29.png/768px-Facebook_Logo_%282019%29.png"
+                            alt=""
+                        />
+                    </a>
+                    <a href="">
+                        <img
+                            src="https://cdn.pixabay.com/photo/2017/08/23/11/30/twitter-2672572_1280.jpg"
+                            alt=""
+                        />
+                    </a>
+                    <a href="">
+                        <img
+                            src="https://img.freepik.com/free-vector/instagram-vector-social-media-icon-7-june-2021-bangkok-thailand_53876-136728.jpg?w=2000"
+                            alt=""
+                        />
+                    </a>
+                    <a href="">
+                        <img
+                            src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+                            alt=""
+                        />
+                    </a>
+                </div>
+            </div>
+        </div>
+  `;
+};
+
+export default Banner;
