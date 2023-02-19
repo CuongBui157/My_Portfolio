2ffc2e6a5b7dadbf9b0879472af3ba8e20fe989c
Cường Bùi <cuongbtph26367@fpt.edu.vn>
2023-02-19 19:04:03 +0700

    https://github.com/CuongBui157/My_Portfolio.git

--- main.js
+++ main.js
@@ -1,12 +1,1 @@
-import "./css/index.css";
-import { render, router } from "./lib";
-import Header from "./components/Header";
-import Skill from "./components/Skill";
-
-const header = document.querySelector("#header");
-const skill = document.querySelector("#skill");
-
-router.on("/", () => render(Header(), header));
-router.on("/", () => render(Skill(), skill));
-
-router.resolve();
+import "./src/css/index.css";
